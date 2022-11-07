207dbed70a519e583939bda2fb74148b2cdc4820
Pablo S <pspinola@stickk.com>
2022-11-07 17:33:55 -0500

    adding introduction to header, fixing buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,25 @@ const Header = () => {
   return (
     <div className="header">
       <h1>Ana Torroja's Studio Albums</h1>
-      <h2>Discover the works of an icon.</h2>
+      <h2>
+        Explore the discography of one of the most iconic Spanish singers of all
+        time.
+      </h2>
+      <h4>
+        Ana Torroja is especially known for being Mecano's lead singer. Once the
+        trio was dissolved in early 1998 –leaving an incalculable legacy for
+        music in Spanish–, the Madrid native decided to embark on a career as a
+        soloist to demonstrate more of her own talent.
+      </h4>
+      <h4>
+        Ana's unique voice has made her a coveted target for music producers
+        from all music genres: whether it's the ethereal 1997 debut 'Puntos
+        Cardinales' (produced by Tony Mansfield and recorded in Abbey Road) or
+        her latest experimentations with retrofuturistic electro-pop, house and
+        disco in her 2021 'Mil Razones' album (co-produced by rising Spanish
+        stars Rosalía, El Guincho and Alizzz), her eclectic discography is worth
+        paying tribute to.
+      </h4>
     </div>
   )
 }
