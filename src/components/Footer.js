2ff5acf94ebf24196496aa918b57d41ac5bdb3d5
Pablo S <pspinola@stickk.com>
2022-11-07 18:28:23 -0500

    reordering footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,20 @@ const Footer = () => {
   return (
     <div className="footer">
       <h2>Useful Links</h2>
+      <a
+        className="footerLinks"
+        href="https://music.apple.com/us/playlist/ana-torroja-bestest/pl.u-BJdNC1VeeoK"
+      >
+        Curated playlist
+      </a>
       <a className="footerLinks" href="http://www.anatorrojaoficial.com/">
         Official Website
       </a>
       <a className="footerLinks" href="http://www.anatorrojaoficial.com/videos">
         Videos
       </a>
-      <a
-        className="footerLinks"
-        href="https://music.apple.com/us/playlist/ana-torroja-bestest/pl.u-BJdNC1VeeoK"
-      >
-        Curated playlist
+      <a className="footerLinks" href="http://www.instagram.com/ana_torroja">
+        Instagram
       </a>
       <h1>by PSF</h1>
       <h3>for General Assembly - FEWD</h3>
