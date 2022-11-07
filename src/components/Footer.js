27c16cf6ea8a081a9784e68ec3256c62c923b496
Pablo S <pspinola@stickk.com>
2022-11-07 16:58:10 -0500

    adding links and flexbox style to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,21 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>by PSF</h1>
       <h2>Useful Links</h2>
+      <a className="footerLinks" href="http://www.anatorrojaoficial.com/">
+        Official Website
+      </a>
+      <a className="footerLinks" href="http://www.anatorrojaoficial.com/videos">
+        Videos
+      </a>
+      <a
+        className="footerLinks"
+        href="https://music.apple.com/us/playlist/ana-torroja-bestest/pl.u-BJdNC1VeeoK"
+      >
+        Curated playlist
+      </a>
+      <h1>by PSF</h1>
+      <h3>for General Assembly - FEWD</h3>
     </div>
   )
 }
